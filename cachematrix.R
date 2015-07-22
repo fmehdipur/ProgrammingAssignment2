5de9cfb873caab62bc469e1f2eb06519bf124455
unknown <farhad@ejust.kyushu-u.ac.jp>
2015-07-22 21:30:29 +0900

    ADDED HI TAG

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
+## This function creates a matrix 
 makeCacheMatrix <- function(x = matrix()) {
 
+ m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+       
 }
 
 
-## Write a short comment describing this function
+## This function calculates the mean of the matrix created with the above function. 
+## It first checks to see if the mean has already been calculated. If so, it gets the mean from the cache and skips the computation. 
+## Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+ mat <- x$getsolve()
+  if(!is.null(mat)) {
+    message("getting cached data")
+    return(mat)
+  }
+  data <- x$get()
+  mat <- solve(data, ...)
+  x$setsolve(mat)
+  mat
+
 }
